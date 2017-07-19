31c8a2b64df15dcfb89e8682c99b6c4a85938fbe
LiangShan <389894467@qq.com>
2017-07-19 23:29:53 +0800

    first_commit
    first

---
+++ app/src/main/java/com/lei/repair/HomeAcitivty.java
@@ -0,0 +1,13 @@
+package com.lei.repair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeAcitivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_acitivty);
+    }
+}
